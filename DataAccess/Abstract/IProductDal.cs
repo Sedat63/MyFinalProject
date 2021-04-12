8ef84e06dbe497133c534d6989b0392441b7e6b3
Sedat Ozturk <sedatozturk63@yandex.com>
2021-04-12 19:29:13 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+	//İlk önce interfacesini oluştur. IProductDal ne demektir?
+	// I = interface, Product = Veritabanında hangi tablo olduğunu, Dal = Hangi katmana karşılık geldiğini
+	//DAL = Data Access Layer
+	public interface IProductDal //Interface nin kendisi default public olmadığından public eklenir
+	{
+		//Interface metodları default public olduğundan public eklenmez.
+		List<Product> GetAll();
+		void Add(Product product);
+		void Update(Product product);
+		void Delete(Product product);
+
+		List<Product> GetAllByCategory(int categoryId); //ürünleri kategoriye göre filtrele
+	}
+}
