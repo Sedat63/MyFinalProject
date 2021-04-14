bc260f77aaffc062fb38966e14cf4e52fb55d0e3
Sedat Ozturk <sedatozturk63@yandex.com>
2021-04-14 17:56:17 +0300

    3 Şubat Kurs sonu

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -8,14 +8,10 @@ namespace DataAccess.Abstract
 	//İlk önce interfacesini oluştur. IProductDal ne demektir?
 	// I = interface, Product = Veritabanında hangi tablo olduğunu, Dal = Hangi katmana karşılık geldiğini
 	//DAL = Data Access Layer
-	public interface IProductDal //Interface nin kendisi default public olmadığından public eklenir
+	public interface IProductDal:IEntityRepository<Product> 
+	//Interface nin kendisi default public olmadığından public eklenir
 	{
 		//Interface metodları default public olduğundan public eklenmez.
-		List<Product> GetAll();
-		void Add(Product product);
-		void Update(Product product);
-		void Delete(Product product);
-
-		List<Product> GetAllByCategory(int categoryId); //ürünleri kategoriye göre filtrele
+		
 	}
 }
