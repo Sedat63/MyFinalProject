9ceaadbca1078f27e768f1a2e65eb677ae93d179
Sedat Ozturk <sedatozturk63@yandex.com>
2021-04-16 23:42:58 +0300

    6 Şubat 2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,11 +1,11 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
-{
+namespace Core.DataAccess //Core: Evrensel projeleri barındırır. Bu nedenle.Net Core ile ilgisi yoktur!! 
+{// Önemli: Core katmanı diğer katmanları referans almaz!!
 	//generic constraint
 	//class: referans tip
 	//IEntity: IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Core.DataAccess;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace DataAccess.Abstract
 {
 	//İlk önce interfacesini oluştur. IProductDal ne demektir?
 	// I = interface, Product = Veritabanında hangi tablo olduğunu, Dal = Hangi katmana karşılık geldiğini
 	//DAL = Data Access Layer
-	public interface IProductDal:IEntityRepository<Product> 
+	public interface IProductDal:IEntityRepository<Product>
 	//Interface nin kendisi default public olmadığından public eklenir
+	//Interface metodları default public olduğundan public eklenmez.
 	{
-		//Interface metodları default public olduğundan public eklenmez.
-		
+		List<ProductDetailDto> GetProductDetails();
 	}
 }
