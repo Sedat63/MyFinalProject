6df41ccbd315951c0532cbc70f2f6301c0bb3a18
Sedat Ozturk <sedatozturk63@yandex.com>
2021-05-02 22:12:17 +0300

    17 Şubat

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 
 namespace Business.Concrete
 {
@@ -22,13 +26,11 @@ namespace Business.Concrete
 			_productDal = productDal;
 		}
 
+		[ValidationAspect(typeof(ProductValidator))] //ad metodunu doğrula(validationaspect) ProductValidator kodlarını kullanarak
 		public IResult Add(Product product)
 		{
-			//iş kodları yazılır.
-			if (product.ProductName.Length<2)
-			{
-				return new ErrorResult(Messages.ProductNameInvalid);
-			}
+			//business codes
+
 			_productDal.Add(product);
 
 			return new SuccessResult(Messages.ProductAdded);
